060d6c2a64a68e19bec5f3acd59fd0d10cb98b7f
Daniel Haanpaa <dhaanpaa@djhent.us>
2021-10-02 20:17:17 -0700

    Initial Setup

---
+++ WorldGen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorldGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
